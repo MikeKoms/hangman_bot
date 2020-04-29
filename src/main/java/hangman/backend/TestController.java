4c271c1eb5d7f29038e67b2f5dc272d4dba063c8
MikeKoms <ponilol37@gmail.com>
2020-04-29 23:31:54 +0300

    restcontroller

--- src/main/java/hangman/backend/TestController.java
+++ src/main/java/hangman/backend/TestController.java
@@ -2,8 +2,10 @@ package hangman.backend;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+
 public class TestController {
 
     @GetMapping("/api/v1/test")
